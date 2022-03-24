313576a37c3b8cc51d2a5527fb5e2f716d2d2c64
Saptarshi Biswas <mathsaptarshi@gmail.com>
2022-03-25 00:00:24 +0530

    Implicit and Explicit binding

--- play-ground-problem.js
+++ play-ground-problem.js
@@ -1,3 +1,34 @@
+// Fetch and show data in a list from this endpoint https://jsonplaceholder.typicode.com/todos
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit  {
+  name = 'Angular';
+  apiUrl = 'https://www.techiediaries.com/api/data.json';
+
+  constructor(private httpClient: HttpClient){}
+
+  ngOnInit(){
+    this.fetchData();
+  }
+
+  private fetchData(){
+    const promise = this.httpClient.get("https://jsonplaceholder.typicode.com/todos").toPromise();
+    console.log(promise);  
+    promise.then((data)=>{
+      console.log("Promise resolved with: " + JSON.stringify(data));
+    }).catch((error)=>{
+      console.log("Promise rejected with " + JSON.stringify(error));
+    });
+  }
+}
+
+
 /* 
 
 1. Create a parent and its child component, add a button in parent component. On clicking the button .Change the color of child component.
