25cdec6c95836105c0830b4fb26a816643bcbe2e
Saptarshi Biswas <mathsaptarshi@gmail.com>
2022-04-02 09:59:16 +0530

    add ts

---
+++ typescript.ts
@@ -0,0 +1,20 @@
+// Q> Define a class Person whit two variable name and age and one method getBirthYear()
+
+class Person {
+  constructor(
+    public name: string,
+    public age: number
+  ) {
+    
+  }
+
+  public getBirthYear() {
+    return new Date().getFullYear() - this.age;
+  }
+}
+
+const jon = new Person("John", 35);
+
+console.log(jon.name);
+console.log(jon.age);
+console.log(jon.getBirthYear());
