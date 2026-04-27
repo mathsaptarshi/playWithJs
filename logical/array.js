4560e1c3d0679e047e9b8f3550fa386fc08f24dc
Saptarshi Biswas <mathsaptarshi@gmail.com>
2026-04-27 19:01:35 +0530

    Updated

--- logical/array.js
+++ logical/array.js
@@ -1,4 +1,25 @@
-let arr = [5,4,4,3,2,3,4,5,5,6,8];
+let arr = [5,4,4,3,2,3,4,5,6,8];
+
+function insert(value,position){
+  for(let i=arr.length-1;position<=i;i--){
+    if(i>=position){
+      arr[i+1] = arr[i];
+      if(i==position){
+        arr[i]= value
+      }
+    }
+  }
+}
+
+function deleteElm(position){
+  for(let i= 0;i<=arr.length-1;i++){
+    if(i>=position){
+      arr[i]=arr[i+1]
+    }
+  }
+  arr.length = arr.length -1
+  console.log(arr)
+}
 
 function maxProd(arr){
   // console.warn(arr.sort((a,b)=>b-a))
@@ -11,6 +32,10 @@ function unique(arr){
   return [...unique]
 }
 
+
+
+insert(11,2);
+deleteElm(2);
 console.log(arr);
 // console.log(maxProd(arr));
 // console.log(unique(arr));
