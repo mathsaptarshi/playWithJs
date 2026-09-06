35f2ab12c93a7fcaf43344f8778a5cde868c1a21
Saptarshi Biswas <mathsaptarshi@gmail.com>
2026-09-07 01:15:18 +0530

    BST

---
+++ tree/binaryTree.js
@@ -0,0 +1,23 @@
+class Node{
+  constructor(val){
+    this.value = val;
+    this.left = null;
+    this.righT = null;
+  }
+}
+
+class BinaryTree{
+  constructor(root){
+    this.root = null;
+  }
+
+  isTreeEmpty(){
+    return this.root == null;
+  }
+
+  createTree(val){
+    let newNode = new Node(val);
+    if(root.value)
+  }
+
+}
